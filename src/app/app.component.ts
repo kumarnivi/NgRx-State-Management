21d9f7ca9188c0ae42c29861f2ca05e49ecf409a
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-21 21:47:13 +0530

    initial Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CounterOutputComponent, CounterControlsComponent],
 })
-export class AppComponent {
-  title = 'NgRx-State-Management';
-}
+export class AppComponent {}
