dad0009abcda78649c6de5628872a2d6eab01562
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-23 16:25:12 +0530

    add ngrx effect

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { provideStore } from '@ngrx/store';
 
 import { AppComponent } from './app/app.component';
 import { counterReducer } from './app/store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 // platformBrowserDynamic().bootstrapModule(AppComponent)
 //   .catch(err => console.error(err));
 
  bootstrapApplication(AppComponent, {
-  providers:[provideStore({counter: counterReducer})] /** this is the global store */
+  providers:[provideStore({ counter: counterReducer }), provideEffects()] /** this is the global store */ /** this is the global store */
  }) 
